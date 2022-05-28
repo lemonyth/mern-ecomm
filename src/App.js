662333e2ce563682660b40112b903dd932c69c08
Onesmo Kasongo <iamonesmo@gmail.com>
2022-05-28 15:43:21 +0530

    homepage components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import Announcement from "./components/Announcement";
-import Slider from "./components/Slider";
 import Home from "./pages/Home";
-import Categories from "./components/Categories";
-import Products from "./components/Products";
-import Newsletter from "./components/Newsletter";
-import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
-      <Announcement />
       <Home />
-      <Slider />
-      <Categories />
-      <Products />
-      <Newsletter />
-      <Footer />
     </div>
   );
 };
