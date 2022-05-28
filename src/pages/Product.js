401b3b3b18dd99e5ab8d8da067aed7559476be54
Onesmo Kasongo <iamonesmo@gmail.com>
2022-05-29 01:15:35 +0530

    product details page

---
+++ src/pages/Product.js
@@ -0,0 +1,159 @@
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  display: flex;
+  padding: 50px;
+`;
+
+const ImageContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+const Title = styled.h1`
+  font-weight: 200;
+`;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 20px 0;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FilterTitle = styled.span`
+  font-weight: 200px;
+  font-size: 20px;
+`;
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+  align-items: center;
+`;
+const QuantityContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 700;
+`;
+const Quantity = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: teal solid 1px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5px;
+`;
+const Button = styled.button`
+  padding: 15px;
+  background-color: white;
+  font-weight: 500;
+  border: 2px solid teal;
+  cursor: pointer;
+  &:hover {
+    background-color: #f8f4f4;
+  }
+`;
+
+function Product() {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImageContainer>
+          <Image src="https://asset.promod.com/product/136712-su-1615293089.jpg?auto=webp&quality=80&width=1920" />
+        </ImageContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Desc>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+            voluptas eos aperiam, iste voluptatibus quidem officia quasi minima
+            aliquam odit sequi distinctio soluta corporis illo a dolor suscipit
+            hic voluptate? Nobis quam eum doloremque deserunt enim? Veniam quasi
+            reprehenderit est animi? Fugit rerum, deserunt eos totam quam veniam
+            officia est?
+          </Desc>
+          <Price>$ 30</Price>
+          {/* <Button>Add to Cart</Button> */}
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <QuantityContainer>
+              <RemoveIcon />
+              <Quantity>1</Quantity>
+              <AddIcon />
+            </QuantityContainer>
+            <Button>Add to Cart</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+}
+
+export default Product;
