563aa5c0409898d42b10d29b8cd09a15dbe2628c
Onesmo Kasongo <iamonesmo@gmail.com>
2022-05-29 17:24:02 +0530

    login, register, cart pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Product from "./pages/Product";
-import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
+// import Product from "./pages/Product";
+// import Home from "./pages/Home";
+// import ProductList from "./pages/ProductList";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <div>
-      <Product />
+      <Cart />
     </div>
   );
 };
---
+++ src/pages/Login.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.pexels.com/photos/1536619/pexels-photo-1536619.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
+  ")
+      center;
+  background-size: cover;
+`;
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0px;
+  padding: 10px;
+  margin-bottom: 10px;
+`;
+const Link = styled.a`
+  font-size: 12px;
+  text-decoration: underline;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  cursor: pointer;
+  color: white;
+  margin: 20px 0px;
+`;
+
+function Login() {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+
+          <Button>LOGIN</Button>
+          <Link>FORGOT PASSWORD?</Link>
+          <Link>CREATE NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+}
+
+export default Login;
